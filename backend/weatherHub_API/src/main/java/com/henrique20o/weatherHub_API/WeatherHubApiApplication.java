407164ad582d3f794837540ba13337d94acb83b3
Henrique20o <henriquepc7@gmail.com>
2026-03-09 20:36:48 -0300

    Initial commit

---
+++ backend/weatherHub_API/src/main/java/com/henrique20o/weatherHub_API/WeatherHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.henrique20o.weatherHub_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherHubApiApplication.class, args);
+	}
+
+}
---
+++ backend/weatherHub_API/src/test/java/com/henrique20o/weatherHub_API/WeatherHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique20o.weatherHub_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
